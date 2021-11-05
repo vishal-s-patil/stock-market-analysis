f7d498acda67ff2b11b7e20b6422a57e214769ba
vishal-s-patil <vishalspolicepatil55@example.com>
2021-11-05 10:40:12 +0530

    updated-search box auto complete

--- index.js
+++ index.js
@@ -13,33 +13,75 @@ app.use(bodyParser.json())
 
 app.use(cors());
 
-
-
 app.post('/login', (req, res) => {
   console.log(req.body);
   connect(req.body);
 })
 
+app.get('/suggestion', (req, response) => {
+  // console.log(req.query.keyword);
+
+  var keywordurl = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${req.query.keyword}&apikey=MURG2M4JADY0CNBX`;
+
+  request.get({
+    url: keywordurl,
+    json: true,
+    headers: {'User-Agent': 'request'}
+  }, (err, res, data) => {
+    if (err) {
+      console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+      console.log('Status:', res.statusCode);
+    } else {
+      response.send(data)  
+      // console.log(data.bestMatches[0]['2. name']);
+    }
+});
+})
 
 
-var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo';
+'use strict';
+var request = require('request');
+const { response } = require('express');
 
-// request.get({
-//     url: url,
-//     json: true,
-//     headers: {'User-Agent': 'request'}
-//   }, (err, res, data) => {
-//     if (err) {
-//       console.log('Error:', err);
-//     } else if (res.statusCode !== 200) {
-//       console.log('Status:', res.statusCode);
-//     } else {
-//       app.get('/', (req, res) => {
-//         res.send(data)
-//       })
-//     }
-// });
+app.get('/getCompanyDetails', (req, response) => {
+  console.log(req.query.cSymbol);
+  var companyDetailsurl = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${req.query.cSymbol}&apikey=MURG2M4JADY0CNBX`;
 
+  console.log(companyDetailsurl);
+
+  request.get({
+    url: companyDetailsurl,
+    json: true,
+    headers: {'User-Agent': 'request'}
+  }, (err, res, data) => {
+    if (err) {
+      console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+      console.log('Status:', res.statusCode);
+    } else {
+      // console.log(data);
+      let CompanyOverview = { 
+                    "Symbol" : data.Symbol,
+                    "company name" : data.Name,
+                    "Country" : data.Country,
+                    "Sector" : data.Sector,
+                    "MarketCapitalization" : data.MarketCapitalization,
+                    "DividendPerShare" : data.DividendPerShare,
+                    "DividendYield" : data.DividendYield,
+                    "52WeekHigh" : data['52WeekHigh'],
+                    "52WeekLow" : data['52WeekLow'],
+                    "50DayMovingAverage" : data["50DayMovingAverage"],
+                    "200DayMovingAverage" : data["200DayMovingAverage"],
+                    "PERatio": data.PERatio,
+                    "PEGRatio": data.PEGRatio,
+                    "BookValue": data.BookValue
+                  }
+      response.send(CompanyOverview)
+      // console.log(CompanyOverview);   
+    }
+});
+})
 
 const uri = "mongodb://127.0.0.1:27017/";
 
--- stock-market-analysis/src/App.js
+++ stock-market-analysis/src/App.js
@@ -1,9 +1,23 @@
 import React, {useState} from 'react'
 function NameForm(){
-
+ 
   const [name,  setName] = useState("")
 
   const [pass,  setPass] = useState("")
+
+  const [searchBox,  setSearchBox] = useState("")
+
+  const [suggestion,  setSuggestion] = useState({
+    bestMatches:
+        [
+        {'2. name' :""},
+        {'2. name' :""},
+        {'2. name' :""},
+        {'2. name' :""},
+        {'2. name' :""},
+        {'2. name' :""},
+        {'2. name' :""},
+        {'2. name' :""}]})
 
   let handleNameChange = e => {
     e.persist();
@@ -15,6 +29,19 @@ function NameForm(){
     setPass(e.target.value);
   };
 
+  let handleSearchBoxChange = e => {
+    e.persist();
+
+    fetch(`http://localhost:3000/suggestion?keyword=${e.target.value}`)
+        .then(res => res.json())
+        .then(data => {
+          setSuggestion(data)
+        });
+        
+        setSearchBox(e.target.value);
+        
+      };
+
   let handleSubmit = (event) => {
 
     fetch('http://localhost:3000/login', {
@@ -25,10 +52,26 @@ function NameForm(){
       
     })
     .then(response => response.json())
-    .then(console.log("hi")).
-    catch(er => console.log(er)); 
+    .catch(er => console.log(er)); 
     
     event.preventDefault();
+  }
+  
+  let handleSuggestionClick = (e) => {
+    console.log(e.target.textContent);
+    console.log(suggestion);
+    for (let i = 0; i < suggestion.bestMatches.length; i++) {
+      if (e.target.textContent.split(",")[0].trim() === suggestion.bestMatches[i]['2. name']){
+        // console.log(suggestion.bestMatches[i]['1. symbol'], suggestion.bestMatches[i]['2. name']);
+        let symbol = suggestion.bestMatches[i]['1. symbol'];
+        // console.log(symbol);
+        fetch(`http://localhost:3000/getCompanyDetails?cSymbol=${symbol}`)
+        .then(res => res.json())
+        .then(data => console.log(data));
+        break;
+      }
+    }
+    e.preventDefault();
   }
   
   return (
@@ -39,8 +82,21 @@ function NameForm(){
           <input type="text" name="name" value={name || ''} onChange={handleNameChange}/>
           pass:
           <input type="text" name="pass" value={pass || ''} onChange={handlePssChange}/>
+          search:
+          <input type="text" name="searchBox" value={searchBox || ''} onChange={handleSearchBoxChange}/>
         </label>
         <input type="submit" value="Submit" />
+        <h1>hi</h1>
+        <ul>
+          <li onClick={handleSuggestionClick}>{suggestion && suggestion.bestMatches[0]['2. name']} , {suggestion.bestMatches[0]['1. symbol']}</li>
+          <li onClick={handleSuggestionClick}>{suggestion && suggestion.bestMatches[1]['2. name']} , {suggestion.bestMatches[1]['1. symbol']}</li>
+          <li onClick={handleSuggestionClick}>{suggestion && suggestion.bestMatches[2]['2. name']} , {suggestion.bestMatches[2]['1. symbol']}</li>
+          <li onClick={handleSuggestionClick}>{suggestion && suggestion.bestMatches[3]['2. name']} , {suggestion.bestMatches[3]['1. symbol']}</li>
+          <li onClick={handleSuggestionClick}>{suggestion && suggestion.bestMatches[4]['2. name']} , {suggestion.bestMatches[4]['1. symbol']}</li>
+          <li onClick={handleSuggestionClick}>{suggestion && suggestion.bestMatches[5]['2. name']} , {suggestion.bestMatches[5]['1. symbol']}</li>
+          <li onClick={handleSuggestionClick}>{suggestion && suggestion.bestMatches[6]['2. name']} , {suggestion.bestMatches[6]['1. symbol']}</li>
+          <li onClick={handleSuggestionClick}>{suggestion && suggestion.bestMatches[7]['2. name']} , {suggestion.bestMatches[7]['1. symbol']}</li>
+        </ul>
       </form>
     </>
   )
